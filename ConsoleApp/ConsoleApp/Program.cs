018d245cfa9a1524b0675e3dadfa6a3ea5a1f585
Pedro Albani <pedroalbani@gmail.com>
2019-03-12 07:27:13 -0300

    Pegando novidades do g1

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,13 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Connectors.G1GamesScraping scraping = new Connectors.G1GamesScraping();
-            Console.WriteLine(scraping.SearchGameNews());
+
+            dynamic dynJson = JsonConvert.DeserializeObject(scraping.SearchGameNews());
+            foreach (var item in dynJson)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            } 
         }
     }
 }
--- ConsoleApp/ConsoleApp/casperjs/bin/g1GameCrawl.js
+++ ConsoleApp/ConsoleApp/casperjs/bin/g1GameCrawl.js
@@ -7,15 +7,19 @@ var casper = require('casper').create({
         loadPlugins: true         // use these settings
     },
     logLevel: "info",              // Only "info" level messages will be logged
-    verbose: true
-
-
+    verbose: true,
+    viewportSize: {
+        width: 1920,
+        height: 1080
+    },
+    
 });
 var x = require('casper').selectXPath;
 var fs = require('fs');
 
 var tempPath;
 var captureSreen = false;
+var fileData;
 
 var screenshot = function () {
     var oDate = new Date();
@@ -42,94 +46,34 @@ var log = function (value) {
     console.log("_______________________________________");
 };
 
-casper.start("https://g1.globo.com/pop-arte/games/");
-casper.then(function () {
+
+ 
+
+casper.start('https://g1.globo.com/pop-arte/games/', function() {
+ 
+
     captureSreen = this.cli.get(0);
     tempPath = this.cli.get(1);
+    fileData = this.cli.get(2);
+
+    log(this.getTitle());
+
+    var json = this.evaluate(function () {
+        var $rows = document.querySelectorAll('.bastian-feed-item');
+        var data = [];
+        for (var i = 0, len = $rows.length; i < len; i++) {
+            var line = $rows[i].querySelector('a').innerText;
+            line += " - " + $rows[i].querySelector('.feed-post-body-resumo').innerText;
+            data.push(line.replace(/\n/g, ' '));
+        }
+        return data;
+    });
+
+    log(json);
+
+    fs.write(fileData, JSON.stringify(json), 'w');
+
 });
-casper.then(function login() {
-    screenshot();
-});
-//casper.then(function () {
-//    screenshot();
-//    this.sendKeys('#j_username', userName);
-//    this.sendKeys('#j_password', userPass);
-//    this.thenClick('input[type=submit]');
-
-//    this.waitForSelector("#sapCustomerSelectId",
-//        function pass() {
-//            log("Found #sapCustomerSelectId");
-//        },
-//        function fail() {
-//            log("Did not load element #sapCustomerSelectId");
-//        },
-//        10000
-//    );
-
-//});
-//casper.then(function () {
-//    screenshot();
-//    this.click(x('//span[text()="Saved Files"]'));
-
-//    this.waitForSelector(".FormattedTableFirstColNoPad",
-//        function pass() {
-//            log("Found .FormattedTableFirstColNoPad");
-//        },
-//        function fail() {
-//            log("Did not load element .FormattedTableFirstColNoPad");
-//        },
-//        10000 // timeout limit in milliseconds;
-//    );
-
-//});
-//casper.then(function () {
-//    screenshot();
-//    links = this.evaluate(function () {
-//        var $rows = document.querySelectorAll("#negotContainer table tbody tr");
-
-//        var data = [];
-//        for (var i = 0, len = $rows.length; i < len; i++) {
-//            if ($rows[i].querySelectorAll('td')[0].className.toString() === "FormattedTableFirstColNoPad") {
-
-//                var name;
-//                var xls;
-
-//                var error = "none";
-//                try {
-//                    name = $rows[i].cells[0].querySelectorAll('a')[0].innerText + ".xls";
-//                    xls = $rows[i].cells[7].querySelectorAll('a')[0].href;
-//                }
-//                catch (e) {
-//                    error = e;
-//                }
-
-//                data.push({
-//                    name: name,
-//                    xls: xls,
-//                    error: error
-//                });
-
-//            }
-
-
-//        }
-
-//        return data;
-//    });
-
-//    fs.write(filePath, JSON.stringify(links), 'w');
-
-//    log(links);
-//});
-//casper.then(function () {
-//    screenshot();
-//    var count = 1;
-//    links.forEach(function (link) {
-//        log(link.xls);
-//        casper.download(link.xls, tempPath + "downloads/" + link.name);
-//    });
-
-//});
 casper.then(function () {
     this.exit(1);
 });
